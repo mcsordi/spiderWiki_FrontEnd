37e819f3eb3ec07af3abffee671b1ca05f18586c
mcsordi <matheuscarissordi@gmail.com>
2024-02-22 21:08:33 -0300

    favorite film concluded

--- favorite.js
+++ favorite.js
@@ -6,3 +6,69 @@ houseDiv.addEventListener("click", (evt) => {
   document.querySelector("#heartDiv i").style.color = "#fff";
   window.open("./index.html", "_self");
 });
+
+const getFavoriteFilms = async () => {
+  const filmEndpoint = "https://spiderwiki-production.up.railway.app/";
+  const idFilm = await fetch(filmEndpoint);
+  const idFilmJson = await idFilm.json();
+  const favoritebackgroundContainer = document.querySelector(
+    ".favoritebackgroundContainer"
+  );
+  const myIdFilms = JSON.parse(localStorage.getItem("favoriteFilmsStorage@Id"));
+
+  const containerFilms = document.createElement("div");
+  containerFilms.setAttribute("class", "containerFilms");
+  const centralizeFilmContainer = document.createElement("div");
+  centralizeFilmContainer.setAttribute("class", "centralizeFilmContainer");
+  const errorContainer = document.createElement("div");
+  errorContainer.setAttribute("class", "errorContainer");
+  const titleFavorite = document.createElement("h1");
+  const rowTitleInfo = document.createElement("div");
+  rowTitleInfo.setAttribute("class", "rowTitleInfo");
+  const iconFavorite = document.createElement("img");
+  iconFavorite.setAttribute(
+    "src",
+    "./src/img/spider-man-anime-circle-shapes-wallpaper-preview.png"
+  );
+  titleFavorite.innerHTML = "Favoritos";
+  rowTitleInfo.appendChild(iconFavorite);
+  rowTitleInfo.appendChild(titleFavorite);
+  favoritebackgroundContainer.appendChild(rowTitleInfo);
+
+  const myUrlImage = () => {
+    try {
+      idFilmJson.map((elApi) => {
+        myIdFilms.map((elFilm) => {
+          if (elApi._id == elFilm) {
+            const myFilmElement = document.createElement("div");
+
+            const imageEl = document.createElement("img");
+            imageEl.setAttribute("src", elApi.image_url);
+            imageEl.setAttribute("id", elApi._id);
+            myFilmElement.appendChild(imageEl);
+            centralizeFilmContainer.appendChild(myFilmElement);
+            containerFilms.appendChild(centralizeFilmContainer);
+
+            favoritebackgroundContainer.appendChild(containerFilms);
+          }
+        });
+      });
+      const allFilmElements = [
+        ...document.querySelectorAll(".centralizeFilmContainer img"),
+      ];
+      allFilmElements.map((el) => {
+        el.addEventListener("click", (evt) => {
+          localStorage.setItem("nameFilm@Id", evt.target.id);
+          window.open("./films.html", "_self");
+        });
+      });
+    } catch {
+      errorContainer.innerHTML =
+        `<img alt="errorImage" src="./src/img/6046-200 (1).png"> ` +
+        "<p>Nenhum Favorito Encontrado</p>";
+      favoritebackgroundContainer.appendChild(errorContainer);
+    }
+  };
+  myUrlImage();
+};
+getFavoriteFilms();
--- films.js
+++ films.js
@@ -1,14 +1,14 @@
 const filmPoster = document.getElementById("filmContent");
 const backArrow = document.getElementById("backArrow");
-const buttonFilm = document.getElementById("buttonFilm");
+const buttonFilm = document.querySelector("buttonFilm");
 
-const renderInfoFilm = async (enpoint, elementId, display) => {
+const renderInfoFilm = async (elementId, display) => {
   const playButton = document.querySelector(".rowInfoIcons button");
   playButton.style.display = display;
   const details = document.getElementById("details");
   const filmDetails = document.getElementById("filmDetails");
   const title = document.getElementById("title");
-  const items = await fetch(enpoint);
+  const items = await fetch("https://spiderwiki-production.up.railway.app/");
   const jsonItems = await items.json();
   await jsonItems.map((el) => {
     if (el._id == elementId) {
@@ -20,20 +20,15 @@ const renderInfoFilm = async (enpoint, elementId, display) => {
       filmDetails.innerHTML = `${el.description}`;
     }
   });
+  buttonFilm.addEventListener("click", (evt) => {
+    window.open("./trailer.html", "_self");
+  });
 };
-const onClickArrow = () => {
+const onClickArrow = (el) => {
   backArrow.addEventListener("click", (evt) => {
     window.open("./index.html", "_self");
   });
 };
 
-buttonFilm.addEventListener("click", (evt) => {
-  window.open("./trailer.html", "_self");
-});
 onClickArrow();
-renderInfoFilm(
-  "https://spiderwiki-production.up.railway.app/",
-
-  localStorage.getItem("nameFilm@Id"),
-  "flex"
-);
+renderInfoFilm(localStorage.getItem("nameFilm@Id"), "flex");
